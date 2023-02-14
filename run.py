b26d214a36fe086bb48171c344eb26cd999f457b
Jason Toudt <125237711+JasonToudt@users.noreply.github.com>
2023-02-13 21:16:02 -0500

    Add Comments to Code
    Added comments to provide clarity

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
+#Import packages from discord_bot directory
 from app.discord_bot.discord_api import client, discord_token
 
+#Run class object, and pass discord_token to initiate the bot connection
+#Checks whether the code is being run as the main program, and not being imported as a module
 if __name__ == '__main__':
     client.run(discord_token)
